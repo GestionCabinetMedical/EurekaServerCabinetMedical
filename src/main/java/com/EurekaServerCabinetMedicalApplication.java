c5f5195bed33f0e60ec6fe6a7b0ab87ac2d4690a
Pauline-Humbert <42661326+Pauline-Humbert@users.noreply.github.com>
2020-11-18 14:07:02 +0100

    initialisation eurekaserver

---
+++ src/main/java/com/EurekaServerCabinetMedicalApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerCabinetMedicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerCabinetMedicalApplication.class, args);
+	}
+
+}
